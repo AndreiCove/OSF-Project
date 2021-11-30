d0f68553d2b766b9d4feb817a5c2a0e4189456be
AndreiCove <94721594+AndreiCove@users.noreply.github.com>
2021-11-30 08:13:41 +0200

    Add files via upload

--- product-img.js
+++ product-img.js
@@ -41,3 +41,27 @@ function myFunction() {
     moreText.style.display = "inline";
   }
 }
+
+
+// 
+
+var i = 0;
+
+function incNumber() {
+    if (i < 10) {
+        i++;
+    } else if (i = 10) {
+        i = 0;
+    }
+    document.getElementById("display").innerHTML = i;
+}
+
+function decNumber() {
+    if (i > 0) {
+        --i;
+    } else if (i = 0) {
+        i = 10;
+    }
+    document.getElementById("display").innerHTML = i;
+}
+
